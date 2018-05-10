32517244b1209e7a9b7051dcacaed170c7c5e034
DimaAdmin <ryba-mech@yandex.ru>
2018-05-10 10:42:14 +0300

    02 - import HttpClient, edit @Component and class  "simple-http" TS

--- src/app/simple-http/simple-http.component.ts
+++ src/app/simple-http/simple-http.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-simple-http',
-  templateUrl: './simple-http.component.html',
-  styleUrls: ['./simple-http.component.css']
+  templateUrl: './simple-http.component.html'
 })
 export class SimpleHttpComponent implements OnInit {
+  data: Object;
+  loading: boolean;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
   }
