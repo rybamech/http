13ebec1b28dbb296587794a118fc20f5cf4926a2
DimaAdmin <ryba-mech@yandex.ru>
2018-05-10 13:31:25 +0300

    06 - create you-tube-search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SimpleHttpComponent } from './simple-http/simple-http.component';
+import { YouTubeSearchComponent } from './you-tube-search/you-tube-search.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleHttpComponent
+    SimpleHttpComponent,
+    YouTubeSearchComponent
   ],
   imports: [
     BrowserModule,
