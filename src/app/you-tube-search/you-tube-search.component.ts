da3f60d136d2c1c4e4ca415fa4d0508c8f7e0889
DimaAdmin <ryba-mech@yandex.ru>
2018-05-10 18:16:34 +0300

    13 - complete you-tube-search.component.ts

--- src/app/you-tube-search/you-tube-search.component.ts
+++ src/app/you-tube-search/you-tube-search.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SearchResult} from './search-result.model';
 
 @Component({
   selector: 'app-you-tube-search',
-  templateUrl: './you-tube-search.component.html',
-  styleUrls: ['./you-tube-search.component.css']
+  templateUrl: './you-tube-search.component.html'
 })
 export class YouTubeSearchComponent implements OnInit {
+  results: SearchResult[];
+  loading: boolean;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  updateResults(results: SearchResult[]): void {
+    this.results = results;
+    // console.log("results:", this.results); // uncomment to take a look
+  }
 }
