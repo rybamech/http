2d4a4a7145c836efcfd4ce01f6a85d28b2b87bd8
DimaAdmin <ryba-mech@yandex.ru>
2018-05-11 10:52:26 +0300

    15 - create new component more-http-requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SimpleHttpComponent } from './simple-http/simple-http.component';
 import { YouTubeSearchComponent } from './you-tube-search/you-tube-search.component';
-
+import { SearchResultComponent } from './you-tube-search/search-result.component';
+import { SearchBoxComponent } from './you-tube-search/search-box.component';
+import { youTubeSearchInjectables } from './you-tube-search/you-tube-search.injectables';
+import { MoreHttpRequestsComponent } from './more-http-requests/more-http-requests.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpleHttpComponent,
-    YouTubeSearchComponent
+    YouTubeSearchComponent,
+    SearchResultComponent,
+    SearchBoxComponent,
+    MoreHttpRequestsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule // <-- right here
   ],
-  providers: [youTubeServiceInjectables],
+  providers: [youTubeSearchInjectables],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
